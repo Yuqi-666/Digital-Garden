fcff8d102f22c9ea9991e17e9207150688e2f7e0
Lyq <3480445425@qq.com>
2025-12-16 20:07:13 +0800

    built wxmlyq-space

--- apps/wxmlyq-space/next.config.ts
+++ apps/wxmlyq-space/next.config.ts
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // 关键：指定静态导出模式
+  images: {
+    unoptimized: true, // 禁用 Next.js 图片优化（静态导出不支持）
+  },
+};
 
 export default nextConfig;
